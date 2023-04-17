bde82720858278c6268460677c0331a4984c8eff
Svyatoslav Glukhov <svyatgluhov@gmail.com>
2023-04-17 13:10:30 +0300

    ADD: other client functionality

--- src/App.vue
+++ src/App.vue
@@ -4,22 +4,75 @@ import { onBeforeMount, ref } from 'vue';
 
 const socket = io('http://localhost:5555');
 
-const messages = ref([]);
+const messages = ref([{}]);
+const messageText = ref('');
+const joined = ref(false);
+const name = ref('');
+const typingDisplay = ref('');
 
 onBeforeMount(() => {
   socket.emit('findAllMessages', (data) => {
     messages.value = data;
   });
+
+  socket.on('message', (message) => {
+    messages.value.push(message);
+  });
+
+  socket.on('typing', ({name, isTyping}) => {
+    if (isTyping) {
+      typingDisplay.value = `${name.name} is typing...`;
+    } else {
+      typingDisplay.value = '';
+    }
+  });
 });
+
+const join = () => {
+  socket.emit('join', { name: name.value }, () => {
+    joined.value = true;
+  });
+};
+
+const sendMessage = () => {
+  socket.emit('createMessage', {text: messageText.value}, () => {
+   messageText.value = '';
+  });
+}
+
+let timeout;
+const emitTyping = () => {
+  socket.emit('typing', {isTyping: true});
+  setTimeout(() => {
+    socket.emit('typing', {isTyping: false});
+  }, 2000);
+}
 </script>
 
 <template>
   <div class="chat">
-    <div class="chat-container">
+    <div v-if="!joined">
+      <form @submit.prevent="join">
+        <label>What's your name?</label>
+        <input v-model="name" />
+        <button type="submit">Send</button>
+      </form>
+    </div>
+    <div class="chat-container" v-else>
       <div class="messages-container">
         <div v-for="message in messages">
-          [{{ message.name }}]: {{ message.text }}
+          [{{ message.name.name }}]: {{ message.text }}
         </div>
+      </div>
+      <div v-if="typingDisplay">{{ typingDisplay }}</div>
+
+      <hr>
+      <div class="message-input">
+        <form @submit.prevent="sendMessage">
+          <label>Message:</label>
+          <input v-model="messageText" @input="emitTyping" />
+          <button type="submit">Send</button>
+        </form>
       </div>
     </div>
   </div>
@@ -27,4 +80,19 @@ onBeforeMount(() => {
 
 <style scoped>
 @import './assets/main.css';
+
+.chat {
+  padding: 20px;
+  height: 50vh;
+}
+
+.chat-container {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+}
+
+.messages-container {
+  flex: 1;
+}
 </style>
